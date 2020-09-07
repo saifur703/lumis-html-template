b9c883a6facdf26187800d39aa093f571d540f56
saifur703 <saifur703@gmail.com>
2020-09-07 09:30:19 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lumis-html-template
---
+++ assets/js/active.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+
+  jQuery(document).ready(function ($) {
+    // Code
+  });
+
+  jQuery(window).on('load', function () {
+    // Code
+  });
+})(jQuery);
